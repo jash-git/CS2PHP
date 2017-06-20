3197db3ced63a4c1cbe5916e51571d5244a35288
jash-git <jash.liao@gmail.com>
2017-06-20 13:16:41 +0800

    Create https://github.com/jash-git/CS2PHP.git

---
+++ Console_CS2PHP/Console_CS2PHP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace Console_CS2PHP
+{
+
+    class Program
+    {
+        static void Pause()
+        {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080/showpostdata.php";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            //request.CookieContainer = cc;
+
+            string firstname = "晅晢"; //用戶名
+            string lastname = "廖"; //密碼
+            string data = "firstname=" + HttpUtility.UrlEncode(firstname) + "&lastname=" + HttpUtility.UrlEncode(lastname);
+            request.ContentLength = data.Length;
+
+            StreamWriter writer = new StreamWriter(request.GetRequestStream(), Encoding.ASCII);
+            writer.Write(data);
+            writer.Flush();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8"; //預設編碼
+            }
+
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            data = reader.ReadToEnd();
+            //cc = request.CookieContainer;
+            response.Close();
+
+            Console.WriteLine(data);
+            Pause();
+        }
+    }
+}
---
+++ PHP/showpostdata.php
@@ -0,0 +1,14 @@
+﻿<?php
+	header('content-type:text/html;charset=utf-8');
+
+	echo "<font size='24' face='Arial'>";//PHP放大字體
+
+	$firstname = htmlspecialchars($_POST['firstname']);
+	$lastname = $_POST['lastname'];
+
+	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+	fwrite($myfile, $lastname);
+	fwrite($myfile, $firstname);
+	fclose($myfile);
+	echo 'OK';
+?>
